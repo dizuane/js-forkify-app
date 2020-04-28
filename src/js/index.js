d852688c1a095f8ddb017b1f6026922bff0fa32e
Duane Spainhour <duane.spainhour@gmail.com>
2020-04-28 05:57:21 -0400

    Added search functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,41 @@
-import x from './test';
-const num = 23;
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, elementStrings, renderLoader, clearLoader } from './views/base';
 
-console.log(`Imported ${x} from another module! Variable num is ${num}`);
+// Global state
+const state = {
+    search: '' // search model instance
+}
+
+const controlSearch = async () => {
+    const query = searchView.getInput();
+
+    if (query) {
+        state.search = new Search(query);
+
+        searchView.clearResults();
+        searchView.clearInput();
+
+        renderLoader(elements.searchResults);
+
+        await state.search.getResults();
+
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResultsPages.addEventListener('click', e => {
+    const btn = e.target.closest(`.${elementStrings.paginationButton}`);
+
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
