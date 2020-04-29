52782d069b37e3aec08b6a4f974bb3ad6553fbe6
Duane Spainhour <duane.spainhour@gmail.com>
2020-04-29 13:05:32 -0400

    Added shopping cart feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, elementStrings, renderLoader, clearLoader } from './views/base';
 
 // Global state
 const state = {};
-
+//window.state = state; //for testing
 const controlSearch = async () => {
     const query = searchView.getInput();
 
@@ -57,6 +59,15 @@ const controlRecipe = async () => {
     }
 };
 
+const controlList = () => {
+    if (!state.list) state.list = new List();
+
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+};
+
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
@@ -85,5 +96,21 @@ elements.recipe.addEventListener('click', e => {
     } else if (e.target.matches('.btn-increase, .btn-increase *')) {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
 });
+
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        state.list.deleteItem(id);
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count--value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
+window.l = new List();
